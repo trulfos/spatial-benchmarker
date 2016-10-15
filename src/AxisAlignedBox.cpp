7d6585b227a8c19e949a96541743ce8aaa320b10
Truls Rustad Fossum <trulfos@gmail.com>
2016-10-15 14:42:46 +0200

    Check for more errors in AxisAlignedBox

--- src/AxisAlignedBox.cpp
+++ src/AxisAlignedBox.cpp
@@ -3,8 +3,23 @@
 AxisAlignedBox::AxisAlignedBox(const Point& a, const Point& b)
 	: points({a, b})
 {
-	//TODO: Check dimension match
-	//TODO: Check that a < b for all coordinates
+	unsigned dimension = a.getDimension();
+
+	// Check point dimensions match
+	if (dimension != b.getDimension()) {
+		throw new std::logic_error(
+				"Dimensions of points for axis aligned box do not match"
+			);
+	}
+
+	// Check point domination
+	for (unsigned i = 0; i < dimension; i++) {
+		if (a.getCoordinates()[i] > b.getCoordinates()[i]) {
+			throw new std::logic_error(
+					"Point a passed to axis aligned box does not dominate point 2"
+				);
+		}
+	}
 }
 
 const std::pair<const Point&, const Point&> AxisAlignedBox::getPoints() const
