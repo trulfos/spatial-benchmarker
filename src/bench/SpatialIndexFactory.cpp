d6ea3292f2947ada469165b46d211bdc02ad6c67
Truls Rustad Fossum <trulfos@gmail.com>
2016-10-15 14:44:50 +0200

    WIP: R-tree index

--- src/bench/SpatialIndexFactory.cpp
+++ src/bench/SpatialIndexFactory.cpp
@@ -2,6 +2,8 @@
 
 #include "NaiveSpatialIndex.hpp"
 #include "ParallelSpatialIndex.hpp"
+#include "RtreeSpatialIndex.hpp"
+
 
 SpatialIndexFactory::SpatialIndexFactory(
 		const std::vector<DataObject>& dataSet
@@ -15,6 +17,8 @@ std::shared_ptr<SpatialIndex> SpatialIndexFactory::create(
 		return std::make_shared<NaiveSpatialIndex>(this->dataSet);
 	} else if (algorithm == "parallel") {
 		return std::make_shared<ParallelSpatialIndex>(this->dataSet);
+	} else if (algorithm == "rtree") {
+		return std::make_shared<RtreeSpatialIndex>(this->dataSet);
 	}
 
 	throw std::invalid_argument(algorithm + " is not a valid algorithm name");
