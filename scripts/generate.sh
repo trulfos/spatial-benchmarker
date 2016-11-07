f4f7c161f25d923233ba2d9f011e6217947a312a
Truls Rustad Fossum <trulfos@gmail.com>
2016-11-07 20:37:27 +0100

    Upgrade generate script
    Now also generates range queries

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,7 +3,7 @@
 # Automates the creation of n-dimensional query sets
 # Usage: generate.sh <#dimensions> <#points> <name>
 
-if (( $# != 3 )); then
+if (( $# != 4 )); then
 	echo "Illegal number of arguments.";
 	exit 1;
 fi;
@@ -24,16 +24,35 @@ mkdir $DST;
 bin/mkdata -d $1 -n $2 > $DST/data.csv;
 
 echo " - Creating query set";
-echo "$1	10" > $DST/queries.csv;
 
-for i in {1..10}; do
-	echo -n "knn	$i" >> $DST/queries.csv;
+if [ "$3" == "knn" ]; then
+	echo "$1	10" > $DST/queries.csv;
 
-	for j in $(seq 1 $1); do
-		echo -n "	0.5" >> $DST/queries.csv;
+	for i in {1..10}; do
+		echo -n "knn	$i" >> $DST/queries.csv;
+
+		for j in $(seq 1 $1); do
+			echo -n "	0.5" >> $DST/queries.csv;
+		done;
+		echo '' >> $DST/queries.csv;
 	done;
-	echo '' >> $DST/queries.csv;
-done;
+else
+	echo "$1	5" > $DST/queries.csv;
+	
+	for i in 0.45 0.46 0.47 0.48 0.49; do
+		echo -n "range" >> $DST/queries.csv;
+
+		for j in $(seq 1 $1); do
+			echo -n "	$i" >> $DST/queries.csv;
+		done;
+
+		for j in $(seq 1 $1); do
+			echo -n "	"$(echo "scale=1;1-$i" | bc) >> $DST/queries.csv;
+		done;
+
+		echo '' >> $DST/queries.csv;
+	done;
+fi;
 
 echo " - Generating valid results";
 bin/bench -a naive -b $DST --no-check -r results > $DST/results.csv;
