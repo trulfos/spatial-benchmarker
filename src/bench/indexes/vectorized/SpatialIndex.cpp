7b7e9728da02730012c2fd7be9d37ceed41411cc
Truls Rustad Fossum <trulfos@gmail.com>
2016-11-08 21:06:13 +0100

    Vectorized knn
    Has a different data layout than the range scan. Must either be consolidated or
    split out.

--- src/bench/indexes/vectorized/SpatialIndex.cpp
+++ src/bench/indexes/vectorized/SpatialIndex.cpp
@@ -51,7 +51,6 @@ SpatialIndex::SpatialIndex(LazyDataSet& dataSet)
 			"Vectorized is currently adapted for floats"
 		);
 
-	//TODO: Make sure there is 4 left at the end
 	positions = aligned_alloc<Coordinate>(
 			8 * sizeof(Coordinate),
 			8 * ((dimension * nObjects - 1) / 8 + 1) * sizeof(Coordinate),
@@ -68,7 +67,7 @@ SpatialIndex::SpatialIndex(LazyDataSet& dataSet)
 
 		const Point& point = object.getPoint();
 		for (unsigned j = 0; j < dimension; j++) {
-			positions[i + j * nObjects] = point[j];
+			positions[8 * (dimension * (i / 8) + j) + i % 8] = point[j];
 		}
 
 		i++;
@@ -152,7 +151,86 @@ Results SpatialIndex::rangeSearch(const AxisAlignedBox& box) const
 
 Results SpatialIndex::knnSearch(unsigned k, const Point& point) const
 {
-	throw std::runtime_error("knnSearch is not implemented for vectorized");
+	float best = std::numeric_limits<float>::infinity();
+
+	// Construct priority queue
+	struct QueueEntry {
+		float distance;
+		DataObject::Id id = 0;
+
+		QueueEntry(float distance, DataObject::Id id)
+			: distance(distance), id(id)
+		{};
+	};
+
+	std::priority_queue<
+			QueueEntry,
+			std::vector<QueueEntry>,
+			std::function<bool(const QueueEntry&, const QueueEntry&)>
+		> queue (
+			[](const QueueEntry& a, const QueueEntry& b) -> bool {
+				return a.distance < b.distance ||
+					(a.distance == b.distance && a.id < b.id);
+			}
+		);
+
+#	pragma omp parallel for schedule(static)
+	for (unsigned b = 0; b < nObjects / 8; ++b) {
+
+		// Sum up contributions from each dimension
+		__m256 sum = _mm256_setzero_ps();
+		for (unsigned d = 0; d < dimension; ++d) {
+			__m256 x = _mm256_load_ps(positions + 8 * (b * dimension + d));
+			__m256 y = _mm256_broadcast_ss(&point[d]);
+			__m256 diff = _mm256_sub_ps(x, y);
+			sum = _mm256_fmadd_ps(diff, diff, sum);
+		}
+
+		// Compare with best so far
+		unsigned isBetter = _mm256_movemask_ps(
+				_mm256_cmp_ps(
+					sum,
+					_mm256_broadcast_ss(&best),
+					_CMP_LE_OS
+				)
+			);
+
+		// Skip the rest if not better
+		if (!isBetter) {
+			continue;
+		}
+
+		float distances[8];
+		_mm256_store_ps(distances, sum);
+
+		// Filter and push to queue
+		for (unsigned s = 0; s < 8; ++s) {
+
+			if (!((isBetter >> s) & 1)) {
+				continue;
+			}
+
+#			pragma omp critical (vector_queue_critical)
+			if (distances[s] < best && (8 * b + s) < nObjects) {
+				queue.emplace(distances[s], ids[8 * b + s]);
+				if (queue.size() > k) {
+					queue.pop();
+					best = queue.top().distance;
+				}
+			}
+
+		}
+	}
+
+	// Construct results
+	Results results;
+
+	while (!queue.empty()) {
+		results.push_back(queue.top().id);
+		queue.pop();
+	}
+
+	return results;
 };
 
 }
