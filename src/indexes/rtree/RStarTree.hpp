trulfos/spatial-benchmarker

commit 580ff3e47a5b980422428b82b66486017a2e612d

Allow reinsert after split in R*-tree
This allows reinsertion to happen when splitting internal nodes as well
as external nodes.

src/indexes/rtree/RStarTree.hpp
@@ -132,6 +132,7 @@ class RStarTree : public Rtree<Node, m>
 
 					// Split node
 					entry = split(**top, entry);
+					isReinsert = false;
 					++top;
 				}
 

commit 42660c2a59b0b89f2f264ab5583ab4599cc82f3c

Fix reinsert bug for R*-tree
Introduced in 580ff3e47.

src/indexes/rtree/RStarTree.hpp
@@ -124,7 +124,7 @@ class RStarTree : public Rtree<Node, m>
 
 						// Reinsert
 						for (Entry<N>& i : extracted) {
-							insert(i, top - path.rbegin(), true);
+							insert(i, top - path.rbegin() + level, true);
 						}
 
 						return;